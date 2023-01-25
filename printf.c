e720e17fa76ee6b744cf257d504e98b1cdaeeaab
root <root@DESKTOP-9JEN2JG>
2023-01-25 16:56:21 +0300

    Added printf.c

---
+++ printf.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: character string
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0, j = 0, count = 0;
+	char *str;
+
+	if (format == NULL)
+		return (-1);
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				_putchar(va_arg(args, int));
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
