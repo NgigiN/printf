61731c3da3dfa02909244164c313282d62f3254b
root <root@DESKTOP-9JEN2JG>
2023-01-25 16:39:42 +0300

    Added main.h

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+#endif /* MAIN_H */
