3cf93bb6af528b742ed031d95e51d7cc44a66598
root <root@mtoto_wa_nairobi>
2023-01-26 22:24:11 +0300

    Added d and i

--- printf.c
+++ printf.c
@@ -26,12 +26,51 @@ void _printstr(void)
 {
 	id.str = va_arg(id.args, char *);
 	if (id.str == NULL)
-		id.str = " ";
+		id.str = "(null)";
 	id.j = 0;
 	while (id.str[id.j] != '\0')
 	{
 		_putchar(id.str[id.j]);
 		id.j++;
+		id.count++;
+	}
+}
+
+/**
+ * _printint - prints integer
+ * 
+ * Return: void
+*/
+
+void _printint(void)
+{
+	int num, div, len;
+
+	num = va_arg(id.args, int);
+	div = 1;
+	len = 0;
+	if (num < 0)
+	{
+		_putchar('-');
+		num = num * -1;
+		id.count++;
+	}
+	if (num == 0)
+	{
+		_putchar('0');
+		id.count++;
+	}
+	while (num / div > 0)
+	{
+		div = div * 10;
+		len++;
+	}
+	div = div / 10;
+	while (len > 0)
+	{
+		_putchar(((num / div) % 10) + '0');
+		div = div / 10;
+		len--;
 		id.count++;
 	}
 }
@@ -59,6 +98,8 @@ int _printf(const char *format, ...)
 				_printchar();
 			else if (format[id.i] == 's')
 				_printstr();
+			else if (format[id.i] == 'd' || format[id.i] == 'i')
+				_printint();
 			else if (format[id.i] == '%')
 			{
 				_putchar('%');
